d70ce4d88bf6eedbe8a9a80243b46267bb348b34
Justin Conger <jhconger@GMAIL.COM>
2021-11-22 12:19:37 -0600

    testing

---
+++ strings.js
@@ -0,0 +1,12 @@
+var myName = "justin";
+var aSentence = "Hi" + " my" + " name" + " is " + myName;
+aSentence +=  " and I love Audio Engineering!";
+var anewSentence = "  \nI also love making music when I am not running a stage!";
+var newestSentence = "  \nI also enjoy playing disc golf with my dog in various parks across the state.";
+aSentence += anewSentence += newestSentence
+ console.log(aSentence);
+
+console.log(myName.charAt(0).toUpperCase() + myName.slice(1));
+
+
+

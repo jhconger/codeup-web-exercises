e25feaea4fe6ca8106d7d4b840747314063ee2bf
Justin Conger <jhconger@GMAIL.COM>
2021-12-03 09:50:44 -0600

    first push today

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,10 +1,34 @@
-// for(i = 0; i <= 10; i++) {
-// for(i = 0; i <= 10; i++) {
-function skipOdd(number) {
-    for(i=0; i<=10; i++) {
-        console.log(number + 2)
-    }
-    // prompt("Please enter an odd number between 1 and 50");
-    // alert("You may exit");
+// // for(i = 0; i <= 10; i++) {
+// // for(i = 0; i <= 10; i++)
+//     // prompt("Please enter an odd number between 1 and 50");
+//     // alert("You may exit");
+//
+// var mynumber = 0 ;
+//
+// do {
+//    mynumber = Number(prompt("put in odd number between 1 and 50"))
+//     if(mynumber < 1 || mynumber  > 50) {
+//         alert("Go Home")
+//     } else if (mynumber %2 === 0) {
+//         alert("number is even!")
+//     } else {
+//         alert("all done")
+//         break;
+//     }
+//
+// } while(true)
+//
+// console.log("number to skip is: " + mynumber)
+// for (var i = 1; i <= 50; i+=2) {
+//     if(i === mynumber) {
+//         console.log("yikes, skipping number: " + mynumber)
+//
+//     }
+// }
+
+var theNumber = 2;
+
+while (theNumber <= 65536) {
+    console.log(theNumber)
+    theNumber *= 2
 }
-console.log(skipOdd(1))

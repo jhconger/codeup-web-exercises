030dd994004be97481ae714c33704af49dcb9a87
Justin Conger <jhconger@GMAIL.COM>
2021-12-03 10:27:28 -0600

    first push today

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -3,13 +3,13 @@
 //     // prompt("Please enter an odd number between 1 and 50");
 //     // alert("You may exit");
 //
-// var mynumber = 0 ;
+// var myNumber = 0 ;
 //
 // do {
-//    mynumber = Number(prompt("put in odd number between 1 and 50"))
-//     if(mynumber < 1 || mynumber  > 50) {
+//    myNumber = Number(prompt("put in odd number between 1 and 50"))
+//     if(myNumber < 1 || myNumber  > 50) {
 //         alert("Go Home")
-//     } else if (mynumber %2 === 0) {
+//     } else if (myNumber %2 === 0) {
 //         alert("number is even!")
 //     } else {
 //         alert("all done")
@@ -18,17 +18,33 @@
 //
 // } while(true)
 //
-// console.log("number to skip is: " + mynumber)
+// console.log("number to skip is: " + myNumber)
 // for (var i = 1; i <= 50; i+=2) {
-//     if(i === mynumber) {
-//         console.log("yikes, skipping number: " + mynumber)
-//
+//     if (i === myNumber) {
+//         console.log("yikes, skipping number: " + myNumber)
+//         continue;
 //     }
+//     console.log(i)
+// }3
+
+
+// var theNumber = 2;
+//
+// while (theNumber <= 65536) {
+//     console.log(theNumber)
+//     theNumber *= 2
 // }
 
-var theNumber = 2;
-
-while (theNumber <= 65536) {
-    console.log(theNumber)
-    theNumber *= 2
-}
+//
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// do {
+//     var conesToPurchase = Math.floor(Math.random() * 5) + 1;
+//     if( conesToPurchase <= allCones) {
+//     console.log(conesToPurchase + " conses sold...")
+//     allCones -= conesToPurchase
+//     } else {
+//         console.log("Cannot sell you" + conesToPurchase + " cones I only have " + allCones + "...")
+//     }
+// console.log(allCones)
+//
+// } while(allCones > 0)

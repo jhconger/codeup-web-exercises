2b54bfa24774b2a58034ea6605378ba528a16917
Justin Conger <jhconger@GMAIL.COM>
2021-12-07 15:35:53 -0600

    newest exercise

--- JS/circle.js
+++ JS/circle.js
@@ -27,25 +27,26 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-        circle = {
-        radius: 5,
-
-        getArea: function () {
-            // TODO: complete this method
-            // hint: area = pi * radius^2
-            return (Math.PI * (this.radius ** 2))
-        },
-
-
-        logInfo: function (doRounding) {
-            if ( doRounding === true) {
-                console.log("Area of a circle with radius: " + this.radius + ", is: ", Math.round(this.getArea()));
-            } else {
-                console.log("Area of a circle with radius: " + this.radius + ", is: ", this.getArea());
-            }
-        }
-    };
+    //     circle = {
+    //     radius: 5,
+    //
+    //     getArea: function () {
+    //         // TODO: complete this method
+    //         // hint: area = pi * radius^2
+    //         return (Math.PI * (this.radius ** 2))
+    //     },
+    //
+    //
+    //     logInfo: function (doRounding) {
+    //         if ( doRounding === true) {
+    //             console.log("Area of a circle with radius: " + this.radius + ", is: ", Math.round(this.getArea()));
+    //         } else {
+    //             console.log("Area of a circle with radius: " + this.radius + ", is: ", this.getArea());
+    //         }
+    //     }
+    // };
     // log info about the circle
+    circle.radius = 5
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");

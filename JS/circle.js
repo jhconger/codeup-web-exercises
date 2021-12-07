4a564a97c29b39fb40174557fb9dcd153d1f9691
Justin Conger <jhconger@GMAIL.COM>
2021-12-07 14:00:38 -0600

    new exercise

---
+++ JS/circle.js
@@ -0,0 +1,53 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            return (Math.PI * (this.radius ** 2))
+        },
+
+
+    logInfo: function (doRounding) {
+        if ( doRounding === true) {
+            console.log("Area of a circle with radius: " + this.radius + ", is: ", Math.round(this.getArea()));
+        } else {
+            console.log("Area of a circle with radius: " + this.radius + ", is: ", this.getArea());
+        }
+    }
+    };
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+        circle = {
+        radius: 5,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            return (Math.PI * (this.radius ** 2))
+        },
+
+
+        logInfo: function (doRounding) {
+            if ( doRounding === true) {
+                console.log("Area of a circle with radius: " + this.radius + ", is: ", Math.round(this.getArea()));
+            } else {
+                console.log("Area of a circle with radius: " + this.radius + ", is: ", this.getArea());
+            }
+        }
+    };
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();

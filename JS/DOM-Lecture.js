7ff3db61bda03273dfb8972898ae29c1c5ddceab
Justin Conger <jhconger@GMAIL.COM>
2022-01-13 15:10:57 -0600

    added exercise

--- JS/DOM-Lecture.js
+++ JS/DOM-Lecture.js
@@ -1,4 +1,30 @@
-var promptAnswer = prompt("what yo name is?");
-var promptWeather = prompt("What is the weather like today?")
-var retrievedP = document.getElementById("weather")
-retrievedP.innerHTML = "the weather is" + promptWeather + "today"
+// todolist change color of hello world to coral when we click on the button
+// 1. define element to change
+// 2. define the button listening for click
+// 3. add the event listener to the button
+// 4. define what event is being listened for
+// 5. change the color
+//
+
+
+
+
+var el = document.getElementById( "orange");
+el.addEventListener("click", function (){
+    console.log('TABLE OF CONTENTS WAS CLICKED ON');
+    el.style.color = "orange";
+})
+
+var helloWorldEl= document.getElementsByTagName("h1")[0];
+console.log(helloWorldEl[0]);
+
+var btn = document.getElementById("btn");
+console.log(btn);
+btn.addEventListener("click",function (){
+    console.log('btn was clicked on');
+    helloWorldEl.style.color = "coral"
+})
+document.getElementById('dark-mode').addEventListener("click", function (){
+
+    el.style.color = "orange"; helloWorldEl.style.color = "coral"
+})

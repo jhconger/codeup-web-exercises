87eff7e3cd1e61d1ad731aefdebcf291222d6f07
Justin Conger <jhconger@GMAIL.COM>
2022-01-18 12:12:11 -0600

    added margins to all buttons

---
+++ JS/js-exercises-january.js
@@ -0,0 +1,26 @@
+// 1. Write a JavaScript program that accept two integers and display the larger.Go to the editor
+// Click me to see the solution
+//
+// 2. Write a JavaScript conditional statement to find the sign of product of three numbers. Display an alert box with the specified sign. Go to the editor
+// Sample numbers : 3, -7, 2
+// Output : The sign is -
+// Click me to see the solution
+//
+// 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor
+// Sample numbers : 0, -1, 4
+// Output : 4, 0, -1
+// Click me to see the solution
+//
+// 4. Write a JavaScript conditional statement to find the largest of five numbers. Display an alert box to show the result. Go to the editor
+// Sample numbers : -5, -2, -6, 0, -1
+// Output : 0
+// Click me to see the solution
+//
+// 5. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen. Go to the editor
+// Sample Output :
+//     "0 is even"
+// "1 is odd"
+// "2 is even"
+// ----------
+//     ----------
+//         Click me to see the solution

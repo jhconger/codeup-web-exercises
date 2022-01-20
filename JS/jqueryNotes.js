0c3a1f9e46739752cc8d00e844031bdfe398ca31
Justin Conger <jhconger@GMAIL.COM>
2022-01-20 09:39:50 -0600

    and completed warm up js exercise

--- JS/jqueryNotes.js
+++ JS/jqueryNotes.js
@@ -1,22 +1,84 @@
 // https://api.jquery.com/
 "use strict";
 
-document.getElementById("heading");
+// document.getElementById("heading");
+//
+// let myEl = $("#p-one");
+// console.log(myEl);
+//
+// let heading = $("#heading").text();
+// console.log(heading);
+//
+// let user1 = {
+//     name: "justin",
+//     isLoggedIn: false,
+//     age: 37
+// }
+//
+// let loginStatus = user1.isLoggedIn;
+//
+// if(loginStatus === true){
+//     $("#heading").addClass("border")
+// }
 
-let myEl = $("#p-one");
-console.log(myEl);
+// console.log(10 % 3)
+//
+// for (var i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+//
+// console.log(!!true)
 
-let heading = $("#heading").text();
-console.log(heading);
+// var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
+//
+// // console.log(
+// //     array[array.length - 1]
+// // );
+// console.log(
+//     array.splice(array.indexOf('Ringo'), 0, 'Yoko')
+//
+// );
+// console.log(array);
 
-let user1 = {
-    name: "justin",
-    isLoggedIn: false,
-    age: 37
+
+// console.log(10 % 3);
+//
+//
+// var array = ['John', 'Paul', 'George', 'Ringo', 'Santana'];
+//
+//
+// console.log(array[array.length - 2]);
+
+
+
+// var band = {}
+// band.shred = function() {
+//     console.log('RAAAAAAAAAA!!!!');
+//
+// }
+// console.log(band.shred())
+
+// write a function that accepts a string and checks whether each string is upperCase or lowerCase and returns "letter  [insert letter] is uppercase/lowercase", accordingly.  Bonus: return "is not a character" if it's not a letter
+//
+// input example: "bJIENsdfRAIRVIrsRFAijgRar"
+
+var i = 0;
+var character='';
+let strings = "How are you doing today?";
+const myArray = strings.split("");
+for (i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+
+
+character = strings.charAt(i);
+    if (!isNaN(character * 1)){
+        alert('character is numeric');
+    }else{
+        if (character == character.toUpperCase()) {
+            alert ('upper case true');
+        }
+        if (character == character.toLowerCase()){
+            alert ('lower case true');
+        }
 }
-
-let loginStatus = user1.isLoggedIn;
-
-if(loginStatus === true){
-    $("#heading").addClass("border")
 }

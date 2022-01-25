1f72209d5010cea88d7e8bd0540c716ea51da6f1
Justin Conger <jhconger@GMAIL.COM>
2022-01-25 09:02:54 -0600

    finished exercise, begin bonus

--- JS/jquery_faq.js
+++ JS/jquery_faq.js
@@ -2,12 +2,22 @@ $(document).ready(function() {
 
     $("dd").addClass('visible');
 
-    $("button").click(function(event) {
+    $("#disappear").click(function(event) {
         $("dd").toggleClass('invisible')
-    })
-});
+    });
+
 
 $('dt') .click(function(event) {
     $(this).toggleClass('highlighted');
 });
-
+// $('li').addClass('bold')
+$('h3').click(function(event) {
+    $(this).next().children().toggleClass('bold');
+});
+    $("#change").click(function(event) {
+        $("li:last-child") .toggleClass('highlighted')
+    });
+    $("li").click(function(event) {
+        $(this).siblings().first().toggleClass('blue')
+    });
+});

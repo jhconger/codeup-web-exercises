35e45274e667ef1341f2a9639053791740540b1a
Justin Conger <jhconger@GMAIL.COM>
2022-02-03 08:58:50 -0600

    new api key

--- JS/api keys.js
+++ JS/api keys.js
@@ -1,2 +1,3 @@
 const token = "pk.eyJ1Ijoiamhjb25nZXIiLCJhIjoiY2t5eGhicmV3MGlqZjJ1bXc1MXdzMnlubSJ9.53Uuao-kj-NGMyyhFNJTMA"
 const weathertoken = "6450ed6396197087c7e503f463ddc13e"
+const weatherkey = "1bbf9714b9ac4babbf9714b9acebabca" //twcapikey

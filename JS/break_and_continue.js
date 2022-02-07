1b62116551fe4b499a999d6e1651fe5664cf5d22
Justin Conger <jhconger@GMAIL.COM>
2022-02-07 13:34:48 -0600

    moved directory

--- JS/break_and_continue.js
+++ JS/break_and_continue.js
@@ -1,50 +1,50 @@
-// // for(i = 0; i <= 10; i++) {
-// // for(i = 0; i <= 10; i++)
-//     // prompt("Please enter an odd number between 1 and 50");
-//     // alert("You may exit");
-//
-// var myNumber = 0 ;
-//
-// do {
-//    myNumber = Number(prompt("put in odd number between 1 and 50"))
-//     if(myNumber < 1 || myNumber  > 50) {
-//         alert("Go Home")
-//     } else if (myNumber %2 === 0) {
-//         alert("number is even!")
-//     } else {
-//         alert("all done")
-//         break;
-//     }
-//
-// } while(true)
-//
-// console.log("number to skip is: " + myNumber)
-// for (var i = 1; i <= 50; i+=2) {
-//     if (i === myNumber) {
-//         console.log("yikes, skipping number: " + myNumber)
-//         continue;
-//     }
-//     console.log(i)
-// }3
+for(i = 0; i <= 10; i++) {
+for(i = 0; i <= 10; i++)
+    prompt("Please enter an odd number between 1 and 50");
+    alert("You may exit");
+
+var myNumber = 0 ;
+
+do {
+   myNumber = Number(prompt("put in odd number between 1 and 50"))
+    if(myNumber < 1 || myNumber  > 50) {
+        alert("Go Home")
+    } else if (myNumber %2 === 0) {
+        alert("number is even!")
+    } else {
+        alert("all done")
+        break;
+    }
+
+} while(true)
+
+console.log("number to skip is: " + myNumber)
+for (var i = 1; i <= 50; i+=2) {
+    if (i === myNumber) {
+        console.log("yikes, skipping number: " + myNumber)
+        continue;
+    }
+    console.log(i)
+}3
 
 
-// var theNumber = 2;
-//
-// while (theNumber <= 65536) {
-//     console.log(theNumber)
-//     theNumber *= 2
-// }
+var theNumber = 2;
 
-//
-// var allCones = Math.floor(Math.random() * 50) + 50;
-// do {
-//     var conesToPurchase = Math.floor(Math.random() * 5) + 1;
-//     if( conesToPurchase <= allCones) {
-//     console.log(conesToPurchase + " conses sold...")
-//     allCones -= conesToPurchase
-//     } else {
-//         console.log("Cannot sell you" + conesToPurchase + " cones I only have " + allCones + "...")
-//     }
-// console.log(allCones)
-//
-// } while(allCones > 0)
+while (theNumber <= 65536) {
+    console.log(theNumber)
+    theNumber *= 2
+}
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+do {
+    var conesToPurchase = Math.floor(Math.random() * 5) + 1;
+    if( conesToPurchase <= allCones) {
+    console.log(conesToPurchase + " conses sold...")
+    allCones -= conesToPurchase
+    } else {
+        console.log("Cannot sell you" + conesToPurchase + " cones I only have " + allCones + "...")
+    }
+console.log(allCones)
+
+} while(allCones > 0)
---
+++ JS/map-filter-reduce.js
@@ -0,0 +1,106 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+
+];
+let threeLanguages = users.filter(function (user){
+    return user.languages.length >= 3;
+});
+console.log(threeLanguages)
+
+let emails = users.map(function (user){
+    user = user.email;
+    return user;
+});
+console.log(emails);
+
+let es6emails = users.map(user => user.email);
+
+const yearsOfExperience = users.reduce(function (accumulator, experience) {
+    accumulator.push(experience.yearsOfExperience);
+    return accumulator;
+}, []);
+
+console.log(yearsOfExperience);
+
+let totalYearsOfExperience = users.reduce(function (total, user){
+    let yearsOfExperience = user.yearsOfExperience;
+    return total + yearsOfExperience;
+}, 0);
+
+console.log(totalYearsOfExperience);
+let average = totalYearsOfExperience/ users.length
+console.log(average)
+
+let emailsViaReduce = users.reduce(function (accumulator, user){
+    accumulator.push(user.email);
+    return accumulator;
+}, []);
+
+let longestEmail = users.reduce(function (accumulator, user, index, userArray){
+    if (index === userArray.length -1){
+        accumulator.push(user.email);
+        accumulator.sort(function (email1, email2){
+            return email2.length - email1.length
+        });
+        return accumulator[0];
+    } else {
+        accumulator.push(user.email);
+        return accumulator;
+    }
+
+},[]);
+
+console.log(longestEmail);
+
+let names = users.reduce(function (accumulator, user){
+    accumulator.push(user.name);
+    return accumulator;
+}, []);
+
+let namesString = users.reduce(function (accumulator, user, index, usersArray){
+    if(index === usersArray.length -1) {
+        accumulator.push(user.name);
+        let namesJoin = accumulator.join(', ');
+        namesJoin = "Your instructors are: " + namesJoin + ".";
+        return namesJoin;
+    } else {
+        accumulator.push(user.name);
+        return accumulator;
+    }
+}, []);
+
+console.log(namesString);

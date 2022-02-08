91ccb53432bb37c68d2b9cf10a7521d87d301964
Justin Conger <jhconger@GMAIL.COM>
2022-02-08 12:38:25 -0600

    added github api key

--- JS/api keys.js
+++ JS/api keys.js
@@ -1,3 +1,4 @@
 const token = "pk.eyJ1Ijoiamhjb25nZXIiLCJhIjoiY2t5eGhicmV3MGlqZjJ1bXc1MXdzMnlubSJ9.53Uuao-kj-NGMyyhFNJTMA"
 const weathertoken = "6450ed6396197087c7e503f463ddc13e"
 const weatherkey = "1bbf9714b9ac4babbf9714b9acebabca" //twcapikey
+const githubkey = "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"

94fa1a52ec685c2316abb8c373dd3c7c4e529429
Justin Conger <jhconger@GMAIL.COM>
2022-02-08 09:26:02 -0600

    finished exam (nearly)

--- JS/map-filter-reduce.js
+++ JS/map-filter-reduce.js
@@ -1,106 +1,104 @@
-const users = [
-    {
-        id: 1,
-        name: 'ryan',
-        email: 'ryan@codeup.com',
-        languages: ['clojure', 'javascript'],
-        yearsOfExperience: 5
-    },
-    {
-        id: 2,
-        name: 'luis',
-        email: 'luis@codeup.com',
-        languages: ['java', 'scala', 'php'],
-        yearsOfExperience: 6
-    },
-    {
-        id: 3,
-        name: 'zach',
-        email: 'zach@codeup.com',
-        languages: ['javascript', 'bash'],
-        yearsOfExperience: 7
-    },
-    {
-        id: 4,
-        name: 'fernando',
-        email: 'fernando@codeup.com',
-        languages: ['java', 'php', 'sql'],
-        yearsOfExperience: 8
-    },
-    {
-        id: 5,
-        name: 'justin',
-        email: 'justin@codeup.com',
-        languages: ['html', 'css', 'javascript', 'php'],
-        yearsOfExperience: 9
-    }
+(function() {
 
-];
-let threeLanguages = users.filter(function (user){
-    return user.languages.length >= 3;
-});
-console.log(threeLanguages)
 
-let emails = users.map(function (user){
-    user = user.email;
-    return user;
-});
-console.log(emails);
+    const users = [
+        {
+            id: 1,
+            name: 'ryan',
+            email: 'ryan@codeup.com',
+            languages: ['clojure', 'javascript'],
+            yearsOfExperience: 5
+        },
+        {
+            id: 2,
+            name: 'luis',
+            email: 'luis@codeup.com',
+            languages: ['java', 'scala', 'php'],
+            yearsOfExperience: 6
+        },
+        {
+            id: 3,
+            name: 'zach',
+            email: 'zach@codeup.com',
+            languages: ['javascript', 'bash'],
+            yearsOfExperience: 7
+        },
+        {
+            id: 4,
+            name: 'fernando',
+            email: 'fernando@codeup.com',
+            languages: ['java', 'php', 'sql'],
+            yearsOfExperience: 8
+        },
+        {
+            id: 5,
+            name: 'justin',
+            email: 'justin@codeup.com',
+            languages: ['html', 'css', 'javascript', 'php'],
+            yearsOfExperience: 9
+        }
 
-let es6emails = users.map(user => user.email);
+    ];
+    let threeLanguages = users.filter(user => user.languages.length >=3);
+    console.log(threeLanguages)
 
-const yearsOfExperience = users.reduce(function (accumulator, experience) {
-    accumulator.push(experience.yearsOfExperience);
-    return accumulator;
-}, []);
+    let emails = users.map(user => user.email);
+    console.log(emails);
 
-console.log(yearsOfExperience);
+    const yearsOfExperience = users.reduce((previousVal , currentVal) => {
+        return previousVal + currentVal.yearsOfExperience;
+    }, 0)
+    console.log(yearsOfExperience);
 
-let totalYearsOfExperience = users.reduce(function (total, user){
-    let yearsOfExperience = user.yearsOfExperience;
-    return total + yearsOfExperience;
-}, 0);
+    let average = yearsOfExperience / users.length
+    console.log(average)
 
-console.log(totalYearsOfExperience);
-let average = totalYearsOfExperience/ users.length
-console.log(average)
+    let longestEmail = users.reduce ((previousValue, currentValue) => {
+        if(previousValue.email.length > currentValue.email.length) {
+            return previousValue.email;
+        } else {
+            return currentValue;
+        }
+    });
 
-let emailsViaReduce = users.reduce(function (accumulator, user){
-    accumulator.push(user.email);
-    return accumulator;
-}, []);
+    console.log(longestEmail);
 
-let longestEmail = users.reduce(function (accumulator, user, index, userArray){
-    if (index === userArray.length -1){
-        accumulator.push(user.email);
-        accumulator.sort(function (email1, email2){
-            return email2.length - email1.length
-        });
-        return accumulator[0];
-    } else {
-        accumulator.push(user.email);
-        return accumulator;
-    }
-
-},[]);
-
-console.log(longestEmail);
-
-let names = users.reduce(function (accumulator, user){
-    accumulator.push(user.name);
-    return accumulator;
-}, []);
-
-let namesString = users.reduce(function (accumulator, user, index, usersArray){
-    if(index === usersArray.length -1) {
-        accumulator.push(user.name);
-        let namesJoin = accumulator.join(', ');
-        namesJoin = "Your instructors are: " + namesJoin + ".";
-        return namesJoin;
-    } else {
+    let names = users.reduce(function (accumulator, user) {
         accumulator.push(user.name);
         return accumulator;
-    }
-}, []);
+    }, []);
 
-console.log(namesString);
+    let namesString = users.reduce(function (accumulator, user, index, usersArray) {
+        if (index === usersArray.length - 1) {
+            accumulator.push(user.name);
+            let namesJoin = accumulator.join(', ');
+            namesJoin = "Your instructors are: " + namesJoin + ".";
+            return namesJoin;
+        } else {
+            accumulator.push(user.name);
+            return accumulator;
+        }
+    }, []);
+
+    console.log(namesString);
+
+})();
+
+// Write a function that removes any duplicates from an array.
+//     Input: [2,3,5,8,3,1,2,6,3,2,7,8,2,4,7]
+// expected output: [2,3,5,8,1,6,7,4]
+let nums = [2,3,5,8,3,1,2,6,3,2,7,8,2,4,7];
+//
+
+//
+// nums.forEach(() => {
+//     if (!uniqueNums.includes()) {
+//         uniqueNums.push();
+//     }
+// });
+//
+// console.log(uniqueNums);
+function uniq(nums) {
+    return Array.from(new Set(nums));
+}
+console.log(uniq(nums))
